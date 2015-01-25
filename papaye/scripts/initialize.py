f629f34fdb0aed23495418759b28eb4b74c3192b
Romain Commandé <commande.romain@gmail.com>
2015-01-25 19:25:32 +0100

    Can pass user and password for the admin user to papaye_init script

--- papaye/scripts/initialize.py
+++ papaye/scripts/initialize.py
@@ -1,5 +1,5 @@
+import argparse
 import getpass
-import sys
 import transaction
 
 from papaye.scripts.common import get_settings
@@ -23,26 +23,37 @@ def create_app_root(dbconn):
     zodb_root = get_database_root(dbconn)
     if not '{}_root'.format(APP_NAME) in zodb_root:
         zodb_root['{}_root'.format(APP_NAME)] = Root()
-    if not 'user' in zodb_root['{}_root'.format(APP_NAME)]:
+    if 'user' not in zodb_root['{}_root'.format(APP_NAME)]:
         zodb_root['{}_root'.format(APP_NAME)]['user'] = Root()
-    if not 'repository' in zodb_root['{}_root'.format(APP_NAME)]:
+    if 'repository' not in zodb_root['{}_root'.format(APP_NAME)]:
         zodb_root['{}_root'.format(APP_NAME)]['repository'] = Root()
 
 
-def create_admin_user(dbconn):
-    username = input('username for administrator (default="admin"): ')
-    username = 'admin' if not username or username == '' else username
-    password = getpass.getpass()
+def create_admin_user(dbconn, username, password):
+    if not username:
+        username = input('username for administrator (default="admin"): ')
+        username = 'admin' if not username or username == '' else username
+    if not password:
+        password = getpass.getpass()
     admin = User(username, password, groups=['group:admin'])
     root = user_root_factory(dbconn)
     root[username] = admin
     transaction.commit()
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Initialize Papaye database.')
+    parser.add_argument('file.ini', type=str, nargs='+', help='configuration INI file')
+    parser.add_argument('--user', type=str, help='Admin username')
+    parser.add_argument('--password', type=str, help='Admin password')
+    return parser.parse_args()
+
+
 def main(*argv, **kwargs):
-    settings = get_settings(sys.argv[1])
+    args = parse_arguments()
+    settings = get_settings(getattr(args, 'file.ini'))
     conn = get_connection(settings)
     if not app_root_exists(conn):
         create_app_root(conn)
-    create_admin_user(conn)
+    create_admin_user(conn, args.user, args.password)
     print("Initialization complete!")
