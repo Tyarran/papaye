ecaecc123c61bb7c047b64877b63a8377e8a0540
Romain Commandé <commande.romain@gmail.com>
2017-08-19 19:04:51 +0200

    Configure travis to use sonar on pull requests

---
+++ travis.sh
@@ -0,0 +1,14 @@
+ #tox -e python$TRAVIS_PYTHON_VERSION
+ #coverage xml -i || true
+ #sed -i 's/filename="/filename=".\//g' coverage.xml || true
+ if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+ then
+	 docker run -v `pwd`:/data rcommande/sonar-scanner -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=rcommande -Dsonar.sources=/data/. -Dsonar.projectKey=papaye
+else
+	docker run -v `pwd`:/data rcommande/sonar-scanner  -Dsonar.analysis.mode=preview \
+              -Dsonar.github.pullRequest=$PULL_REQUEST_ID \
+              -Dsonar.github.repository=rcommande/papaye \
+              -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN \
+              -Dsonar.host.url=https://sonarcloud.io/ \
+              -Dsonar.login=$SONAR_TOKEN
+fi
