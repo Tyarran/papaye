9a0546390849f6fc0f39db289416c25c23a15cc1
Romain Commandé <commande.romain@gmail.com>
2017-08-20 21:53:10 +0200

    Update encrypted travis vars

--- travis.sh
+++ travis.sh
@@ -5,7 +5,8 @@ sed -i 's/filename="/filename=".\//g' coverage.xml || true
 if [ "$TRAVIS_PULL_REQUEST" ]
 then
 	echo "sonar-scanner for PR"
-	docker run -v `pwd`:/data rcommande/sonar-scanner -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$PULL_REQUEST_ID -Dsonar.github.repository=rcommande/papaye -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=rcommande
+	docker run -v `pwd`:/data rcommande/sonar-scanner -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$PULL_REQUEST_ID -Dsonar.github.repository=rcommande/papaye -Dsonar.github.oauth=$GITHUB_TOKEN -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=rcommande -Dsonar.sources=/data/. -Dsonar.projectKey=papaye
+
 else
 	echo "sonar-scanner"
 	docker run -v `pwd`:/data rcommande/sonar-scanner -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=rcommande -Dsonar.sources=/data/. -Dsonar.projectKey=papaye
