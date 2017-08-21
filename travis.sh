e12d2f868f233912b6449872c0491df45f7d88f2
Romain Commandé <commande.romain@gmail.com>
2017-08-21 22:14:14 +0200

    Remove useless echo

--- travis.sh
+++ travis.sh
@@ -13,11 +13,9 @@ if [ "$TASK_NUMBER" == 1 ]
 then
 	if [ "$TRAVIS_PULL_REQUEST" ]
 	then
-		echo "sonar-scanner for PR"
 		docker run -v `pwd`:/data -v sonar_cache:/root/.sonar/cache rcommande/sonar-scanner -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$PULL_REQUEST_ID -Dsonar.github.repository=rcommande/papaye -Dsonar.github.oauth=$GITHUB_TOKEN -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=rcommande -Dsonar.sources=/data/. -Dsonar.projectKey=papaye
 
 	else
-		echo "sonar-scanner"
 		docker run -v `pwd`:/data rcommande/sonar-scanner -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=rcommande -Dsonar.sources=/data/. -Dsonar.projectKey=papaye
 	fi
 fi
