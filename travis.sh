7fcc677c1d0e5737658eafd4442b7acea41d74c1
Romain Commandé <commande.romain@gmail.com>
2017-08-21 07:42:16 +0200

    Try to pass encrypted variables to script params

--- travis.sh
+++ travis.sh
@@ -1,11 +1,16 @@
+SONAR_TOKEN=$1
+GITHUB_TOKEN=$2
+PULL_REQUEST_ID=$3
+
 tox -e python$TRAVIS_PYTHON_VERSION
 coverage xml -i || true
-sed -i 's/filename="/filename=".\//g' coverage.xml || true
+sed -i 's/filename="/filename=".\//g' coverage.xml || true 
+
 
 if [ "$TRAVIS_PULL_REQUEST" ]
 then
 	echo "sonar-scanner for PR"
-	docker run -v `pwd`:/data rcommande/sonar-scanner -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$PULL_REQUEST_ID -Dsonar.github.repository=rcommande/papaye -Dsonar.github.oauth=$GITHUB_TOKEN -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=rcommande -Dsonar.sources=/data/. -Dsonar.projectKey=papaye
+	docker run -v `pwd`:/data -v sonar_cache:/root/.sonar/cache rcommande/sonar-scanner -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$PULL_REQUEST_ID -Dsonar.github.repository=rcommande/papaye -Dsonar.github.oauth=$GITHUB_TOKEN -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=rcommande -Dsonar.sources=/data/. -Dsonar.projectKey=papaye
 
 else
 	echo "sonar-scanner"
