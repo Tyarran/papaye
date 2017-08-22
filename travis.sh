81f47c473c2dbf579e29c4a6e88b5d8f533040bb
Romain Commandé <commande.romain@gmail.com>
2017-08-22 07:34:45 +0200

    Fix travis.sh for non PR builds

--- travis.sh
+++ travis.sh
@@ -7,16 +7,27 @@ TASK_NUMBER=$(echo $JOB_NUMBER | cut -d '.' -f2)
 tox -e python$TRAVIS_PYTHON_VERSION
 coverage xml -i || true
 sed -i 's/filename="/filename=".\//g' coverage.xml || true 
-env
-
 
 if [ "$TASK_NUMBER" == 1 ]
 then
-	if [ "$TRAVIS_PULL_REQUEST" ]
+	if [ "$PULL_REQUEST_ID" == 'false' ]
 	then
-		docker run -v `pwd`:/data -v sonar_cache:/root/.sonar/cache rcommande/sonar-scanner -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$PULL_REQUEST_ID -Dsonar.github.repository=rcommande/papaye -Dsonar.github.oauth=$GITHUB_TOKEN -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=rcommande -Dsonar.sources=/data/. -Dsonar.projectKey=papaye
-
+		docker run -v `pwd`:/data rcommande/sonar-scanner \
+			-Dsonar.host.url=https://sonarcloud.io \
+			-Dsonar.login=$SONAR_TOKEN \
+			-Dsonar.organization=rcommande \
+			-Dsonar.sources=/data/. \
+			-Dsonar.projectKey=papaye
 	else
-		docker run -v `pwd`:/data rcommande/sonar-scanner -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=rcommande -Dsonar.sources=/data/. -Dsonar.projectKey=papaye
+		docker run -v `pwd`:/data rcommande/sonar-scanner \
+			-Dsonar.analysis.mode=preview \
+			-Dsonar.github.pullRequest=$PULL_REQUEST_ID \
+			-Dsonar.github.repository=rcommande/papaye \
+			-Dsonar.github.oauth=$GITHUB_TOKEN \
+			-Dsonar.host.url=https://sonarcloud.io \
+			-Dsonar.login=$SONAR_TOKEN \
+			-Dsonar.organization=rcommande \
+			-Dsonar.sources=/data/. \
+			-Dsonar.projectKey=papaye
 	fi
 fi
